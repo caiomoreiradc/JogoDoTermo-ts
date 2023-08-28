6c48532a3898ab53057e6147a4392c0e24330dbd
Tiago <tiagosantiniathayde@gmail.com>
2023-08-28 19:44:50 -0300

    Build v1.0.0

--- dist/js/avaliacao-letra.js
+++
@@ -1,7 +0,0 @@
-export var AvaliacaoLetra;
-(function (AvaliacaoLetra) {
-    AvaliacaoLetra[AvaliacaoLetra["PosicaoCorreta"] = 0] = "PosicaoCorreta";
-    AvaliacaoLetra[AvaliacaoLetra["PosicaoIncorreta"] = 1] = "PosicaoIncorreta";
-    AvaliacaoLetra[AvaliacaoLetra["NaoExistente"] = 2] = "NaoExistente";
-})(AvaliacaoLetra || (AvaliacaoLetra = {}));
-//# sourceMappingURL=avaliacao-letra.js.map
