78bc66cd808840a671b0efce97b1e30b22d32e00
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-09-10 22:18:54 -0300

    Atualizado

--- dist/js/termo.js
+++ dist/js/termo.js
@@ -1,6 +1,13 @@
 export class Termo {
+    constructor() {
+        this.palavraSecreta = 'CAIXA';
+        this.tentativas = 0;
+        console.log(this.palavraSecreta);
+    }
+    avaliarPalavra(palavra) {
+        return "abcde";
+    }
     obterQuantidadeTentativas() {
-        throw new Error("Method not implemented.");
     }
 }
 //# sourceMappingURL=termo.js.map
